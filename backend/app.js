37ea4ff930778edb7fd07f90ee8a64e4f868790d
kok.lim <kok.lim@bytedance.com>
2024-06-13 11:58:46 +0800

    feat: add port specification on .env

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var port = process.env.BACKEND_PORT || 3001
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
